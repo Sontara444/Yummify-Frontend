e7b26e60fdc338ce3f8eaf713baf7471cd7a8232
Sontara444 <sontararajput@gmail.com>
2025-02-10 00:53:12 +0530

    update place order component

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,32 +1,112 @@
-import React, { useContext } from 'react'
-import "./PlaceOrder.css"
-import { AppContext } from '../../context/AppContext'
+import React, { useContext, useState } from "react";
+import "./PlaceOrder.css";
+import { AppContext } from "../../context/AppContext";
 
 const PlaceOrder = () => {
+  const { getTotalCartAmount, token, food_list, cartItems, backendUrl } =
+    useContext(AppContext);
 
-    const {getTotalCartAmount} = useContext(AppContext)
+  const [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    street: "",
+    city: "",
+    state: "",
+    zipcode: "",
+    country: "",
+    phone: "",
+  });
+
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setData((data) => ({ ...data, [name]: value }));
+  };
+
+
+  const placeOrder = async (event)=>{
+    event.preventDefault();
+  }
+
+
 
   return (
-    <form className='place-order'>
-        <div className="place-order-left">
-            <p className="title">Delivery Information</p>
-            <div className="multi-fields">
-                <input type="text"placeholder='first Name'/>
-                <input type="text"placeholder='Last Name'/>
-            </div>
-            <input type="email" placeholder='Email address'/>
-            <input type="text" placeholder='Street'/>
-            <div className="multi-fields">
-                <input type="text"placeholder='City'/>
-                <input type="text"placeholder='State'/>
-            </div>
-            <div className="multi-fields">
-                <input type="text"placeholder='Zip code'/>
-                <input type="text"placeholder='Country'/>
-            </div>
-            <input type="text" placeholder='Phone' />
+    <form onSubmit={placeOrder} className="place-order">
+      <div className="place-order-left">
+        <p className="title">Delivery Information</p>
+        <div className="multi-fields">
+          <input
+            name="firstName"
+            value={data.firstName}
+            onChange={onChangeHandler}
+            type="text"
+            placeholder="First Name"
+          />
+          <input
+            name="lastName"
+            value={data.lastName}
+            onChange={onChangeHandler}
+            type="text"
+            placeholder="Last Name"
+          />
         </div>
-        <div className="place-order-right">
+        <input
+          name="email"
+          value={data.email}
+          onChange={onChangeHandler}
+          type="email"
+          placeholder="Email address"
+        />
+        <input
+          name="street"
+          value={data.street}
+          onChange={onChangeHandler}
+          type="text"
+          placeholder="Street"
+        />
+        <div className="multi-fields">
+          <input
+            name="city"
+            value={data.city}
+            onChange={onChangeHandler}
+            type="text"
+            placeholder="City"
+          />
+          <input
+            name="state"
+            value={data.city}
+            onChange={onChangeHandler}
+            type="text"
+            placeholder="State"
+          />
+        </div>
+        <div className="multi-fields">
+          <input
+            name="zipcode"
+            value={data.zipcode}
+            onChange={onChangeHandler}
+            type="text"
+            placeholder="Zip code"
+          />
+          <input
+            name="country"
+            value={data.country}
+            onChange={onChangeHandler}
+            type="text"
+            placeholder="Country"
+          />
+        </div>
+        <input
+          name="phone"
+          value={data.phone}
+          onChange={onChangeHandler}
+          type="text"
+          placeholder="Phone"
+        />
+      </div>
+      <div className="place-order-right">
         <div className="place-order-total">
           <h1>Cart Totals</h1>
           <div>
@@ -45,11 +125,11 @@ const PlaceOrder = () => {
               <h3>${getTotalCartAmount()}</h3>
             </div>
           </div>
-          <button >PROCEED TO PAYMENT</button>
+          <button type="submit">PROCEED TO PAYMENT</button>
         </div>
-        </div>
+      </div>
     </form>
-  )
-}
+  );
+};
 
-export default PlaceOrder   
+export default PlaceOrder;
